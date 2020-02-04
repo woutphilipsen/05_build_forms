270794a3fe3ba1195cf56610e22ace9ceb000f48
Wout Philipsen <Wout.Philipsen@ad.core.vdabantwerpen.intra>
2020-02-04 11:37:54 +0100

    2. form submitting & rendering list of all articles

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -2,11 +2,13 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use App\Entity\Article;
 use App\Form\ArticleFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,9 +18,25 @@ class ArticleAdminController extends AbstractController
      * @Route("/admin/article/new", name="admin_article_new")
      * @IsGranted("ROLE_ADMIN_ARTICLE")
      */
-    public function new(EntityManagerInterface $em)
+    public function new(EntityManagerInterface $em, Request $request)
     {
         $form = $this->createForm(ArticleFormType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $data = $form->getData();
+            $article = new Article();
+            $article->setTitle($data['title']);
+            $article->setContent($data['content']);
+            $article->setAuthor($this->getUser());
+
+            $em->persist($article);
+            $em->flush();
+
+            return $this->redirectToRoute('app_homepage');
+        }
+
         
         return $this->render('article_admin/new.html.twig', [
             'articleForm' => $form->createView()
@@ -32,5 +50,17 @@ class ArticleAdminController extends AbstractController
     public function edit(Article $article)
     {
         dd($article);
+    }
+
+    /**
+     * @Route("/admin/article")
+     */
+    public function list(ArticleRepository $articleRepo)
+    {
+        $articles = $articleRepo->findAll();
+
+        return $this->render('article_admin/list.html.twig', [
+            'articles' => $articles
+        ]);
     }
 }
