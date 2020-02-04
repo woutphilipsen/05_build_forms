f68a2c3c6a64b13adb6918ac344233a3b3aae06d
Wout Philipsen <Wout.Philipsen@ad.core.vdabantwerpen.intra>
2020-02-04 14:15:29 +0100

    6. types options: define TextType & help-box to address meaning of input

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -2,6 +2,7 @@
 
 namespace App\Form;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -12,7 +13,9 @@ class ArticleFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
+            ->add('title', TextType::class, [
+                'help' => 'Choose something catchy!',
+            ])
             ->add('content')
         ;
     }
