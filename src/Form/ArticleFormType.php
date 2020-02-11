574c9b1228b8600edfa62142ec3e5659fd187cd8
Woutje <philipsenwout@outlook.com>
2020-02-11 10:43:59 +0100

    23. Custom field type Creating a UserSelectTexType

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -30,16 +30,7 @@ class ArticleFormType extends AbstractType
             ->add('publishedAt', null, [
                 'widget' => 'single_text'
             ])
-            ->add('author', EntityType::class, [
-                'class' => User::class,
-                'choice_label' => function(User $user) {
-                    return sprintf('(%d) %s', $user->getId(), $user->getEmail());
-                },
-                'placeholder' => 'Choose an author!',
-                'choices' => $this->userRepository
-                    ->findAllEmailAlphabetical(),
-                'invalid_message' => 'We are too smart for your hacking!'
-            ])
+            ->add('author', UserSelectTextType::class)
         ;
     }
 
