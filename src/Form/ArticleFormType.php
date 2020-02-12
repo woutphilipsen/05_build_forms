52c933fb67bdb7ee9c18763b5f22bb553a47a727
Woutje <philipsenwout@outlook.com>
2020-02-12 11:56:46 +0100

    37. dynamic form events, link the location & the specific location box with each other by using formevents POST_SUBMIT

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -10,6 +10,9 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ArticleFormType extends AbstractType
@@ -62,6 +65,17 @@ class ArticleFormType extends AbstractType
                 'widget' => 'single_text'
             ]);
         }
+
+        $builder->get('location')->addEventListener(
+            FormEvents::POST_SUBMIT,
+            function (FormEvent $event) {
+                $form = $event->getForm();
+                $this->setupSpecificLocationNameField(
+                    $form->getParent(),
+                    $form->getData()
+                );
+            }
+        );
     }
 
     public function configureOptions(OptionsResolver $resolver)
@@ -102,5 +116,26 @@ class ArticleFormType extends AbstractType
         ];
 
         return $locationNameChoices[$location];
+    }
+
+    private function setupSpecificLocationNameField(FormInterface $form, ?string $location)
+    {
+        if (null === $location) {
+            $form->remove('specificLocationName');
+            return;
+        }
+
+        $choices = $this->getLocationNameChoices($location);
+
+        if (null === $choices) {
+            $form->remove('specificLocationName');
+            return;
+        }
+
+        $form->add('specificLocationName', ChoiceType::class, [
+            'placeholder' => 'Where exactly?',
+            'choices' => $choices,
+            'required' => false,
+        ]);
     }
 }
