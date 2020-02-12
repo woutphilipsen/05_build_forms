a11b329cf723845bef3704bedc255793a3f7a58f
Woutje <philipsenwout@outlook.com>
2020-02-12 13:23:27 +0100

    39. dynamic select js with an ajax call, autoupdate the next select box looking at the previous changed select item

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleAdminController extends AbstractController
@@ -64,6 +65,25 @@ class ArticleAdminController extends AbstractController
         }
 
         return $this->render('article_admin/edit.html.twig', [
+            'articleForm' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/article/location-select", name="admin_article_location_select")
+     */
+    public function getSpecificLocationSelect(Request $request)
+    {
+        $article = new Article();
+        $article->setLocation($request->query->get('location'));
+        $form = $this->createForm(ArticleFormType::class, $article);
+
+        // no field? return an empty response
+        if (!$form->has('specificLocationName')) {
+            return new Response(null, 204);
+        }
+
+        return $this->render('article_admin/_specific_location_name.html.twig', [
             'articleForm' => $form->createView()
         ]);
     }
