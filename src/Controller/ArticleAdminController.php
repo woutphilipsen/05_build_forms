2f264a29b7901c9d572722c0c5ecc63f31aeec58
Woutje <philipsenwout@outlook.com>
2020-02-12 14:10:40 +0100

    41. security cleanup

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleAdminController extends AbstractController
+class ArticleAdminController extends BaseController
 {
     /**
      * @Route("/admin/article/new", name="admin_article_new")
@@ -71,9 +71,15 @@ class ArticleAdminController extends AbstractController
 
     /**
      * @Route("/admin/article/location-select", name="admin_article_location_select")
+     * @IsGranted("ROLE_USER")
      */
     public function getSpecificLocationSelect(Request $request)
     {
+        if (!$this->isGranted('ROLE_ADMIN_ARTICLE') && $this->getUser()->getArticles()->isEmpty())
+        {
+            throw $this->createAccessDeniedException();
+        }
+
         $article = new Article();
         $article->setLocation($request->query->get('location'));
         $form = $this->createForm(ArticleFormType::class, $article);
