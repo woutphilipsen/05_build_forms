906aa5ad0b486a4c191eee9e9b03626a3f8a4933
Woutje <philipsenwout@outlook.com>
2020-02-12 13:34:28 +0100

    40. handle the technical fault that was made on interstellar space choice => giv null as setSpecificLocationName

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -289,6 +289,10 @@ class Article
     {
         $this->location = $location;
 
+        if (!$location || $location === 'interstellar_space') {
+            $this->setSpecificLocationName(null);
+        }
+
         return $this;
     }
 
